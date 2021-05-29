84154a6703a61a68929e51a76e4057130ffd5cb1
rikiindramawan <72116921+rikiindramawan@users.noreply.github.com>
2021-05-29 16:03:27 +0700

    update Title to title

--- resources/views/createmateri.blade.php
+++ resources/views/createmateri.blade.php
@@ -10,7 +10,7 @@
                         <form method="POST" action="{{ config('app.url')}}/materi" >
                             @csrf
                             <div class="form-group row">
-                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Title') }}</label>
+                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('title') }}</label>
                                 <div class="col-md-6">
                                 <input type="text" name="titleOfMateri" class="form-control">
                                 </div>
