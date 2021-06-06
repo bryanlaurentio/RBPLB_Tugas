e4e9e9cda909a417e8996e671ac60f9028f52853
KevianwillieHandoyo <kevianwillie.h@gmail.com>
2021-06-06 15:21:16 +0700

    Migrate Membership

---
+++ database/migrations/2021_06_06_074555_create_memberships_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembershipsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('memberships', function (Blueprint $table) {
+            $table->string('nameOfPackage')->primary();
+            $table->integer('price');
+            $table->integer('duration');
+            $table->integer('paymentCode')->unsigned();
+            $table->foreign('paymentCode')->references('paymentCode')->on('payment_methods');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('memberships');
+    }
+}
