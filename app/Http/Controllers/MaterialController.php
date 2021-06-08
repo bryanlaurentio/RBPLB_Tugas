50342a28b29837bd986fce71e7d0cef0e59f2a92
Bryan Laurentio <bryanlaurentio@gmail.com>
2021-06-08 09:23:28 +0700

    Update Skeleton MaterialController

--- app/Http/Controllers/MaterialController.php
+++ app/Http/Controllers/MaterialController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class MaterialsController extends Controller
+class MaterialController extends Controller
 {
     /**
      * Display a listing of the resource.
