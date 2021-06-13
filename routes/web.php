e01da0edfc6ae4607b07401646813421d237053c
KevianwillieHandoyo <kevianwillie.h@gmail.com>
2021-06-13 20:36:42 +0700

    Update Bug Route Profile

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,10 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 // Auth::routes();
-// Route::get('/profile', [App\Http\Controllers\ProfileController::class, 'edit'])->name('home');
+Route::get('/profile', [App\Http\Controllers\ProfileController::class, 'edit'])->name('profile.edit');
+Route::put('/profile', [App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
+Route::put('/profile/password', [App\Http\Controllers\ProfileController::class, 'password'])->name('profile.password');
+
 
 // Auth::routes();
 // Route::get('/home', 'App\Http\Controllers\HomeController@index')->name('home');
@@ -30,12 +33,12 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 Route::group(['middleware' => 'auth'], function () {
 Route::resource('user', 'App\Http\Controllers\UserController', ['except' => ['show']]);
-Route::get('profile', ['as' => 'profile.edit', 'uses' => 'App\Http\Controllers\ProfileController@edit']);
-Route::put('profile', ['as' => 'profile.update', 'uses' => 'App\Http\Controllers\ProfileController@update']);
+//Route::get('profile', ['as' => 'profile.edit', 'uses' => 'App\Http\Controllers\ProfileController@edit']);
+//Route::put('profile', ['as' => 'profile.update', 'uses' => 'App\Http\Controllers\ProfileController@update']);
 Route::get('upgrade', function () {return view('pages.upgrade');})->name('upgrade');
 Route::get('map', function () {return view('pages.maps');})->name('map');
 Route::get('icons', function () {return view('pages.icons');})->name('icons');
 Route::get('table-list', function () {return view('pages.tables');})->name('table');
-Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'App\Http\Controllers\ProfileController@password']);
+//Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'App\Http\Controllers\ProfileController@password']);
 });
 
